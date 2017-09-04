56db278b2ec60fe85167ca6ca22ffc976b54d5b7
Jeno Liu <jeno5980515@yahoo.com.tw>
2017-09-05 00:51:24 +0800

    Update 647 - Palindromic Substrings.js

--- 647 - Palindromic Substrings.js
+++ 647 - Palindromic Substrings.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var countSubstrings = function(s) {
-    console.log(s);
     let count = 0;
     let bool = Array.apply(null, Array(s.length+1)).map(row => new Array(s.length+1).fill(true));
     for ( var i = s.length - 1 ; i >= 0 ; i -- ){
